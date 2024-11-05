a6e09f76d496e454280623c9ac164d2f1eb45e2d
godcandidate <godcandidate101@gmail.com>
2024-11-05 10:52:01 +0000

    create keep_an_idoit_busy.py

---
+++ input_validation/keep_an_idoit_busy.py
@@ -0,0 +1,8 @@
+import pyinputplus as pyip
+
+while 1:
+    option = pyip.inputYesNo("Want to know how to keep an idiot busy for hours: ")
+    if option == 'no':
+        break
+    
+print('Thank you. Have a nice day.')
